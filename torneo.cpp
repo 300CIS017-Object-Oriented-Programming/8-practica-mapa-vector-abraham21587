1384d0c2aa30b96db462a73afbdcce17595556e7
abraham21587 <abrahamwaggon821@gmail.com>
2025-10-25 15:22:25 -0500

    solo faltaria el torneo.cpp y main en torneo cpp coloque la estructura mas no las funciones de cada funcion git push origin main

--- torneo.cpp
+++ torneo.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "torneo.h"
+#include <iostream>
+using namespace std;
+
+void torneo::registrarVideojuego() {
+
+}
+
+void torneo::registrarJugador() {
+
+}
+
+void torneo::inscribirJugadorEnVideojuego() {
+
+}
+
+void torneo::mostrarVideojuegosJugador() {
+
+}
+
+void torneo::inicializarVideojuegos() {
+
+}
+
+void torneo::inicializarJugadores() {
+
+}
+
+
+
+
+
